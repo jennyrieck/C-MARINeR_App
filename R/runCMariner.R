66a851a1c0e47b164ddaeadab1dc887347a876da
Ogan Mancarci <ogan.mancarci@gmail.com>
2019-05-22 11:00:02 -0400

    comment for future

--- R/runCMariner.R
+++ R/runCMariner.R
@@ -1,5 +1,9 @@
 runCMariner = function(array = NULL, design = NULL,...){
 
+  # this is the only way that I know to pass arguments into a shiny app.
+  # a variable with a set name is created in the global environment. this
+  # variable is deleted when execution is finished. May overwrite user data
+  # if variable names clash
   .GlobalEnv$.marinerApp$array = array
   .GlobalEnv$.marinerApp$design = design
   on.exit(rm(.marinerApp, envir=.GlobalEnv))
