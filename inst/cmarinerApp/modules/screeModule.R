a4c4eda1c3f1d235d47551044536c71cb26182aa
jennyrieck <jenny.rieck@gmail.com>
2019-05-22 12:21:35 -0400

    updated scree plotting to include % var labels and eigen labels

--- inst/cmarinerApp/modules/screeModule.R
+++ inst/cmarinerApp/modules/screeModule.R
@@ -8,6 +8,12 @@ screeUI = function(id){
 screeModule = function(input,output,session,analysisResults){
   output$plot = renderPlot({
     print('i am plotted')
-    plot(analysisResults()$compromise_eigen$values,type = 'l', xlab='Component', ylab='Eigen Value',main='Rv Scree Plot')
-  })
+    plot((analysisResults()$compromise_eigen$values/sum(analysisResults()$compromise_eigen$values)*100),type = 'l', xlab='Component', ylab='',main='Compromise Scree Plot', axes=F)
+    box()
+    axis(2,at= (analysisResults()$compromise_eigen$values/sum(analysisResults()$compromise_eigen$values)*100),labels=round(analysisResults()$compromise_eigen$values/sum(analysisResults()$compromise_eigen$values)*100,1),las=2,cex.axis=.65)
+    mtext("Explained Variance",2,line=3)
+    axis(4,at= (analysisResults()$compromise_eigen$values/sum(analysisResults()$compromise_eigen$values)*100),labels=round(analysisResults()$compromise_eigen$values,2),las=2,cex.axis=.65)
+    mtext("eigen Value",4,line=3)
+    axis(1,at=1:length(analysisResults()$compromise_eigen$values),lwd=1)
+          })
 }
