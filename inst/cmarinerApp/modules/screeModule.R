e2542483ff049f581d0972f4be6530deb44d3243
jennyrieck <jenny.rieck@gmail.com>
2019-05-22 12:39:14 -0400

    changed scree plotting margins

--- inst/cmarinerApp/modules/screeModule.R
+++ inst/cmarinerApp/modules/screeModule.R
@@ -8,6 +8,7 @@ screeUI = function(id){
 screeModule = function(input,output,session,analysisResults){
   output$plot = renderPlot({
     print('i am plotted')
+    par(mar=c(5, 5, 4, 5) + 0.1)
     plot((analysisResults()$compromise_eigen$values/sum(analysisResults()$compromise_eigen$values)*100),type = 'l', xlab='Component', ylab='',main='Compromise Scree Plot', axes=F)
     box()
     axis(2,at= (analysisResults()$compromise_eigen$values/sum(analysisResults()$compromise_eigen$values)*100),labels=round(analysisResults()$compromise_eigen$values/sum(analysisResults()$compromise_eigen$values)*100,1),las=2,cex.axis=.65)
